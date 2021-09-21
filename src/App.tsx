cc81d0e5e5a7fe6b6b52499067d1026ac6f35b4c
kritika243 <24kritikasharma24@gmail.com>
2021-09-21 22:44:04 +0530

    nav and links in sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <Router>
+        <Sidebar />
+      </Router>
     </>
   )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,18 @@
 import styles from './Sidebar.module.scss'
 import samanthaImage from '../../assets/png/samantha.png'
+import { Link, useLocation } from 'react-router-dom'
+
+const sidebarNavLinks = [
+  'dashboard',
+  'expences',
+  'wallets',
+  'summary',
+  'accounts',
+  'settings',
+]
 
 export default function Sidebar() {
+  const loacation = useLocation()
   return (
     <>
       <aside className={styles.sidebar}>
@@ -14,6 +25,25 @@ export default function Sidebar() {
             <p className={styles.user_name}>Lisa</p>
             <p className={styles.user_email}>hi.lisa@email.com</p>
           </div>
+          <nav className={styles.sidebar_nav}>
+            <ul>
+              {sidebarNavLinks.map((sidebarNavLink) => (
+                <li className={styles.sidebar_nav_item}>
+                  <Link
+                    className={
+                      loacation.pathname === `/${sidebarNavLink}`
+                        ? styles.sidebar_nav_link_active
+                        : styles.sidebar_nav_link
+                    }
+                    to={`/${sidebarNavLink}`}
+                  >
+                    {sidebarNavLink.charAt(0).toUpperCase() +
+                      sidebarNavLink.slice(1)}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
       </aside>
     </>
